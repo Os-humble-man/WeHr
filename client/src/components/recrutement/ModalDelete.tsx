018f47bf2c7abd8048d9a79a47c408d4d1d2077b
Maurice Miema <mauricemiema90@gmail.com>
2025-02-26 22:09:25 +0100

    Add recruitment table, offer creation form, and delete offer modal

--- client/src/components/recrutement/ModalDelete.tsx
+++ client/src/components/recrutement/ModalDelete.tsx
@@ -15,32 +15,34 @@ function ModalDelete({CloseModalDelete}: ModalDeleteProps) {
                     transition={{ duration: 0.5, ease: "easeInOut" }}
                     className="bg-white px-4 py-2 rounded-lg"
                 >
-                    <div className="flex justify-center">
-                        < FcHighPriority size={70} />
-                    </div>
-                    <div className="w-96 block">
-                        <h1 className="text-xl text-center py-4">Êtes-vous sûr de vouloir supprimer cette offre ?</h1>
-                        <p className="text-center text-gray-400">
-                            La suppression de cette offre est permanente et irréversible. Une fois supprimée, 
-                            elle ne pourra pas être récupérée.
-                        </p>
-                    </div>
+                    <div>
+                        <div className="flex justify-center">
+                            < FcHighPriority size={70} />
+                        </div>
+                        <div className="md:w-96 md:block">
+                            <h1 className="text-xl text-center py-4">Êtes-vous sûr de vouloir supprimer cette offre ?</h1>
+                            <p className="text-center text-gray-400">
+                                La suppression de cette offre est permanente et irréversible. Une fois supprimée, 
+                                elle ne pourra pas être récupérée.
+                            </p>
+                        </div>
 
-                    <div className="flex gap-2 mt-4">
-                        <button 
-                            type="button"
-                            onClick={CloseModalDelete}
-                            className="border border-gray-400 font-medium cursor-pointer rounded-lg py-2 w-full"
-                        >
-                            Cancel
-                        </button>
+                        <div className="flex gap-2 mt-4">
+                            <button 
+                                type="button"
+                                onClick={CloseModalDelete}
+                                className="border border-gray-400 font-medium cursor-pointer rounded-lg py-2 w-full"
+                            >
+                                Cancel
+                            </button>
 
-                        <button 
-                            type="button"
-                            className="bg-red-500 cursor-pointer font-medium text-white rounded-lg py-2 w-full"
-                        >
-                            Delete
-                        </button>
+                            <button 
+                                type="button"
+                                className="bg-red-500 cursor-pointer font-medium text-white rounded-lg py-2 w-full"
+                            >
+                                Delete
+                            </button>
+                        </div>
                     </div>
                 </motion.div>
             </div>
